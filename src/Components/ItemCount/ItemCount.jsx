ee647baae6e1217345bac2eaffbb834c55d24864
ameggiolaro11 <ameggiolaro11@gmail.com>
2024-01-10 01:38:45 -0300

    corrijo errores

--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -20,16 +20,18 @@ export const ItemCount = ({ stock, initial = 1, onAdd }) => {
 
   return (
     <div className="d-flex flex-column col-2 justify-content-center align-content-center p-4">
-      <div>
-        <button className="btn btn-outline-secondary mx-3" onClick={increment}>
+      <div className="d-flex align-items-center">
+        <button className="btn btn-outline-secondary" onClick={increment}>
           +
         </button>
-        <strong>{count}</strong>
-        <button className="btn btn-outline-secondary mx-3" onClick={decrement}>
+        <strong className="mx-3">{count}</strong>
+        <button className="btn btn-outline-secondary" onClick={decrement}>
           -
         </button>
       </div>
-      <button className="btn btn-outline-primary mt-2" onClick={() => onAdd(count)}>Agregar al carrito</button>
+      <button className="btn btn-outline-primary mt-2" onClick={() => onAdd(count)}>
+        Agregar al carrito
+      </button>
     </div>
   );
-};
+  }
